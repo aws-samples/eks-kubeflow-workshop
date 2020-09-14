90954bf52e5e3a6bd4b4b3c12cbb75121643dad2
Jiaxin Shan <seedjeffwan@gmail.com>
2020-09-14 10:13:58 -0700

    Add papermill init test case

---
+++ tests/01_notebook_test.py
@@ -0,0 +1,7 @@
+import papermill as pm
+
+pm.execute_notebook(
+   '../notebooks/01_Jupyter_Notebook/01_01_Notebook_Development.ipynb',
+   'output/01_Notebook_Development.ipynb'
+   )
+
---
+++ tests/README.md
@@ -0,0 +1,8 @@
+## Papermill tests
+
+This folder includes notebook tests cases writtern by papermill.
+Follow following commands to run these tests 
+
+```
+python3 *.py
+```

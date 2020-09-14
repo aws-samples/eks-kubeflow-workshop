df920483e6bda85a03cf81c76b13b3f7313b4bc6
Jiaxin Shan <seedjeffwan@gmail.com>
2020-09-14 10:47:33 -0700

    Change to run tests at root folder

--- tests/01_notebook_test.py
+++ tests/01_notebook_test.py
@@ -1,7 +1,7 @@
 import papermill as pm
 
 pm.execute_notebook(
-   '../notebooks/01_Jupyter_Notebook/01_01_Notebook_Development.ipynb',
-   'output/01_Notebook_Development.ipynb'
+   'notebooks/01_Jupyter_Notebook/01_01_Notebook_Development.ipynb',
+   'tests/output/01_Notebook_Development.ipynb'
    )
 
--- tests/README.md
+++ tests/README.md
@@ -1,8 +1,8 @@
 ## Papermill tests
 
 This folder includes notebook tests cases writtern by papermill.
-Follow following commands to run these tests 
+Follow following commands to run these tests at root folder level
 
 ```
-python3 *.py
+python3 tests/*.py
 ```

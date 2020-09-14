47aa5481b16d42843aad38b3e8eef6a6508b99de
Jiaxin Shan <seedjeffwan@gmail.com>
2020-09-14 11:41:13 -0700

    Write test files in tmp folder

--- tests/01_notebook_test.py
+++ tests/01_notebook_test.py
@@ -2,6 +2,6 @@ import papermill as pm
 
 pm.execute_notebook(
    'notebooks/01_Jupyter_Notebook/01_01_Notebook_Development.ipynb',
-   'tests/output/01_Notebook_Development.ipynb'
+   '/tmp/eks-kubeflow/workshop/test_output/01_Notebook_Development.ipynb'
    )
 
--- tests/output/README.md
+++
@@ -1,1 +0,0 @@
-just used to host output
